72e170e06a8b641a0caf1237957ec22b2713725b
philopon <philopon.dependence@gmail.com>
2014-05-23 15:26:42 +0900

    remove all files. yesod -> apiary.

--- Application/Database.hs
+++
@@ -1,4 +0,0 @@
-module Application.Database
-    ( module Application.Database.PostgreSQL.Compact ) where
-
-import Application.Database.PostgreSQL.Compact
--- Handler/Home.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TupleSections, OverloadedStrings #-}
-module Handler.Home where
-
-import Import
-
-getHomeR :: Handler Html
-getHomeR = defaultLayout $ do
-    setTitle "infopipe"
-    $(widgetFile "homepage")
